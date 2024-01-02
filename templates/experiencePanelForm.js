2be6e449623172fafe953033ee959c24c3ef24cd
JBryskiewicz <kuba@fiku.eu>
2024-01-02 22:27:50 +0100

    template fix to reduce character name length if unecessarily long and style exp input field to increase readability

--- templates/experiencePanelForm.js
+++ templates/experiencePanelForm.js
@@ -1,3 +1,7 @@
+const limitNameLength = (name) => {
+  return name.split(" ")[0].substring(0, 9);
+};
+
 export const experiencePanelForm = (actorList, numberOfActors) => {
   let table = `
   <form>
@@ -7,15 +11,21 @@ export const experiencePanelForm = (actorList, numberOfActors) => {
   for (let i = 0; i < numberOfActors; i++) {
     table += `
     <tr>
-         <td><b>${actorList[i].name}</b></td>
+         <td><b>${limitNameLength(actorList[i].name)}</b></td>
          <td><b>Level:</b> ${actorList[i].system.level.value}</td>
-         <td><b>XP:</b> ${actorList[i].system.level.xp}</td>
+         <td>
+          <b>XP:</b> 
+          ${actorList[i].system.level.xp} 
+          / 
+          ${actorList[i].system.level.value * 10}
+         </td>
          <td>
             <input 
                 type="number" 
                 name="exp${i}" 
                 id="exp${i}" 
-                value="" 
+                value=""
+                style="width: 48px;"
             />
          </td>
      </tr>`;
